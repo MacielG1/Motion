ccf1519ec44481fd5c811fa71ec08b6ad01b40d1
macielg1 <guflumaciel@gmail.com>
2023-10-27 13:24:35 -0300

    added error page and readme.MD

---
+++ src/app/error.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Error() {
+  return (
+    <section className="flex h-full flex-col items-center justify-center gap-4">
+      <Image src="/error.jpg" width="300" height="300" alt="Error" />
+      <p className="text-xl font-semibold">An Error Occurred</p>
+      <Button asChild>
+        <Link href="/documents">Return</Link>
+      </Button>
+    </section>
+  );
+}
